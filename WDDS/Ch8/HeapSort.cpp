af644026f0779ec2ff3937d1b159dc61ebcaf4e6
ALEX-VII <15091076006@163.com>
2024-08-23 10:35:39 +0800

    使用从1开始表示的完全二叉树数组更方便

--- WDDS/Ch8/HeapSort.cpp
+++ WDDS/Ch8/HeapSort.cpp
@@ -1,48 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define N 10
-int nums[N];
-void Init(int* nums){
-    for(int i = 0; i < N; i++) nums[i] = rand() % 10;
-}
-void showResult(int* res){
-    for(int i = 0; i < N; i++) cout << res[i] << "  ";
-    cout << endl; 
-}
-// 这里我们使用的堆是使用数组表示的，可以视为完全二叉树
-// 大根堆
-void HeapAdjust(int* nums,int k,int len){
-    int sentinel = nums[k];
-    for(int i = 2*k; i < len; i*=2){
-        // 跳转大相邻的大的
-        if(i < len-1 && nums[i] < nums[i+1]) i++;
-        if(sentinel >= nums[i]) break;
+int arr[] = {0,9,4,9,7,5,4,5,2,0,9};
+
+void heapadjust(int a[],int k,int len){
+    a[0] = a[k];
+    for(int i = 2*k;i <= len; i*=2){
+        if(i < len && a[i] < a[i+1]) i++;
+        if(a[0] >= a[i]) break;
         else{
-            nums[k] = nums[i];
-            k = i;  //下坠
+            a[k] = a[i];
+            k = i;
         }
     }
-    nums[k] = sentinel; // 太小被放在下面
+    a[k] = a[0];
 }
-void BuildMaxHeap(int* nums){
-    // 这里一定要逆序，不然结果不对，因为先调整后面又调整了
-    for(int i = N/2; i > 0; i--) HeapAdjust(nums,i,N);
+void buildmaxheap(int a[],int len){
+    for(int i = len/2; i > 0; i--) heapadjust(a,i,len);
 }
-void HeapSort(int* nums){
-    BuildMaxHeap(nums);
-    for(int i = N-1; i >= 0; i--){
-        swap(nums[i],nums[0]);
-        HeapAdjust(nums,0,i-1);
+void heapsort(int a[],int len){
+    buildmaxheap(a,len);
+    for(int i = len;i > 1; i--){
+        swap(a[i],a[1]);
+        for(auto i = 1; i <= 10; i++) cout << arr[i];
+        cout << endl;
+        heapadjust(a,1,i-1);
     }
 }
 int main()
 {
-    srand(static_cast<unsigned int>(time(0)));
-    Init(nums);
-    showResult(nums);
-    HeapSort(nums);
-    cout << "Sorted" << endl;
-    showResult(nums);
+    heapsort(arr,10);
     return	0;
 }
-// TODO 有小Bug
